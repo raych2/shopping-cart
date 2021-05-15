0875dd1a305b4233cc292c4c3d9cbbb525dcb9e1
raych2 <rtongco@gmail.com>
2021-05-15 13:36:32 -0700

    Add perfume images and perfumeData to images folder

---
+++ src/images/perfumeData.js
@@ -0,0 +1,91 @@
+import chanel from "./anna-daudelin-m9lnm9jL918-unsplash.jpg";
+import byredo from "./charisse-kenion-uMqMqRy54es-unsplash.jpg";
+import malone from "./charles-deluvio-3IMl0kCxpHQ-unsplash.jpg";
+import viktor from "./guy-basabose-Ex154b30Ows-unsplash.jpg";
+import armani from "./heidi-walley-m6sh2vS0Yjk-unsplash.jpg";
+import givenchy from "./laura-chouette-udYyaBiSTNE-unsplash.jpg";
+import diptyque from "./raygar-he-1T34Rtm41BU-unsplash.jpg";
+import guerlain from "./taisiia-shestopal-yn0OFyyw5ls-unsplash.jpg";
+
+const perfumes = [
+  {
+    id: 1,
+    name: "Chance Eau Tendre",
+    brand: "Chanel",
+    size: "1.7oz/50mL Eau de Parfum Spray",
+    desciption:
+      "An enhanced interpretation of the unexpected floral-fruity fragrance, CHANCE EAU TENDRE Eau de Parfum sweeps you into an intensified whirlwind of tenderness. The signature CHANCE bottle is reinvented with a silver cap and golden accent.",
+    image: chanel,
+    price: 108,
+  },
+  {
+    id: 2,
+    name: "Velvet Haze",
+    brand: "Byredo",
+    size: "3.4oz/100mL Eau de Parfum Spray",
+    desciption:
+      "Velvet Haze evokes the daze state attained in artificial paradises. The opulence of the scent embarks us in a psychedelic whirlpool. Along this exhilarating path, the sweetness of coconut water soon gives way to the feverish power of patchouli leaves. An experience of all senses that the velvety cacao and the wild musk enrich with their earthy envelope.",
+    image: byredo,
+    price: 270,
+  },
+  {
+    id: 3,
+    name: "Nectarine Blossom & Honey Cologne",
+    brand: "Jo Malone London",
+    size: "3.4oz/100mL Spray",
+    desciption:
+      "Inspired by London's Covent Garden early morning market, this fragrance combines succulent notes of nectarine, peach, and cassis with delicate spring flowers that melt into a note of acacia honey. The result is a sweet and delightfully playful fragrance.",
+    image: malone,
+    price: 142,
+  },
+  {
+    id: 4,
+    name: "Flowerbomb",
+    brand: "Viktor&Rolf",
+    size: "3.4 oz/ 100 mL Eau de Parfum Spray",
+    desciption:
+      "This floral perfume for women explodes with an intoxicating whirl of cattleya, jasmine, and rose. The warm scent of patchouli and vanilla grounds the fresh fragrance, adding a sensual twist on the scent reminiscent of a room filled with rose petals.",
+    image: viktor,
+    price: 165,
+  },
+  {
+    id: 5,
+    name: "Si",
+    brand: "Giorgio Armani",
+    size: "3.4 oz/ 100 mL Eau de Parfum Spray",
+    desciption:
+      "A sweet take on a floral fragrance, this women's perfume opens with luminous notes of blackcurrant nectar and mandarin. Orange blossom forms the heart of the fragrance and warm vanilla anchors the irresistible blend for a lasting scent.",
+    image: armani,
+    price: 128,
+  },
+  {
+    id: 6,
+    name: "L'Interdit",
+    brand: "Givenchy",
+    size: "2.6 oz/ 80 mL Eau de Parfum Spray",
+    desciption:
+      "L’Interdit Eau de Parfum is an invitation to defy convention and embrace your individuality. The perfume’s white floral bouquet is cut with wood base notes, unleashing a bold, luminous trail that is long lasting.",
+    image: givenchy,
+    price: 111,
+  },
+  {
+    id: 7,
+    name: "Do Son",
+    brand: "Diptyque",
+    size: "3.4 oz/ 100 mL Eau de Toilette Spray",
+    desciption:
+      "As a child, Yves Coueslant, one of diptyque’s founders, spent his summers in Do Son, in Ha Long Bay. The sea breeze carried the heady and spicy scent of tuberoses. Do Son has the delicateness and persistence of a memory from a childhood in Indochina - the memory of a flower, between lightness and delight.",
+    image: diptyque,
+    price: 145,
+  },
+  {
+    id: 8,
+    name: "Aqua Allegoria Granada Salvia",
+    brand: "Guerlain",
+    size: "4.22 oz/ 125 mL Eau de Parfum Spray",
+    desciption:
+      "Granada Salvia is a sparkling Eau de Toilette recalling a warm Andalusian garden, inviting us to bite into an exquisite pomegranate. This Eau de Toilette embodies the spirit of pomegranate, imbued with a twist of sage over a white musk background.",
+    image: guerlain,
+    price: 140,
+  },
+];
