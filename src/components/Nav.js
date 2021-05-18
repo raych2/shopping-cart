fee0f808a735c960742c7d5481d4ba9cd25283e8
raych2 <rtongco@gmail.com>
2021-05-17 21:59:00 -0700

    Add styled components to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,48 @@
 import React from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
+
+const Navbar = styled.nav`
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #000;
+  color: #fff;
+`;
+const LinkList = styled.ul`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 20vw;
+  list-style-type: none;
+`;
+const NavLink = styled(Link)`
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+  color: #fff;
+`;
 
 const Nav = () => {
   return (
-    <nav className="Nav">
-      <Link to="/">
+    <Navbar>
+      <NavLink to="/">
         <h3>Favorite Perfume Store</h3>
-      </Link>
-      <ul className="nav-links">
-        <Link to="/shop">
+      </NavLink>
+      <LinkList>
+        <NavLink to="/shop">
           <li>Shop</li>
-        </Link>
-      </ul>
-    </nav>
+        </NavLink>
+        <NavLink to="/cart">
+          <li>Cart</li>
+        </NavLink>
+      </LinkList>
+    </Navbar>
   );
 };
 
