f2c8632ce94ad2132bf7771d6ed99ee0a5cbd191
raych2 <rtongco@gmail.com>
2021-05-18 21:44:48 -0700

    Add StyledButton to reuse in other components

---
+++ src/shared/StyledButton.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  border-radius: 3px;
+  padding: 10px;
+  font-size: 1em;
+  text-align: center;
+  width: 20vw;
+`;
+
+export default StyledButton;
