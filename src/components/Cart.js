5515ae21ebc8e33c8714a121442c3cdc904ced25
raych2 <rtongco@gmail.com>
2021-05-25 21:46:00 -0700

    Add display of cartItems or empty cart message to Cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,86 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Header from "../shared/Header";
+import StyledButton from "../shared/StyledButton";
 
-const Cart = () => {
-  return (
-     <div className="Cart">
-         <h1>Cart</h1>
-     </div>
-  )
-}
+const CartCollection = styled.div`
+  display: grid;
+  grid-template-columns: 75vw 25vw;
+`;
+const ItemCollection = styled.div`
+  display: grid;
+  grid-template-row: repeat(1, 1fr);
+  row-gap: 20px;
+`;
+const TotalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const ItemContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 4px;
+  margin-left: 20px;
+`;
+const StyledImage = styled.img`
+  height: 250px;
+  width: auto;
+  border: 5px solid gray;
+`;
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const EmptyCart = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const EmptyMessage = styled.div`
+  font-size: 2em;
+  margin-bottom: 40px;
+  text-align: center;
+`;
+
+const Cart = (props) => {
+  const cartItems = props.cartItems;
+
+  return cartItems.length !== 0 ? (
+    <>
+      <Header>Cart</Header>
+      <CartCollection>
+        <ItemCollection>
+          {cartItems.map((item, index) => {
+            return (
+              <ItemContainer key={index}>
+                <StyledImage
+                  src={item.image}
+                  alt={`${item.brand} ${item.name}`}
+                ></StyledImage>
+                <InfoContainer>
+                  <div>{item.brand}</div>
+                  <div>{item.name}</div>
+                </InfoContainer>
+                <div>${item.price}</div>
+                <div>{item.quantity}</div>
+              </ItemContainer>
+            );
+          })}
+        </ItemCollection>
+        <TotalContainer>$$$</TotalContainer>
+      </CartCollection>
+    </>
+  ) : (
+    <EmptyCart>
+      <Header>Cart</Header>
+      <EmptyMessage>Your cart is currently empty</EmptyMessage>
+      <Link to="/shop">
+        <StyledButton>Return to Shop</StyledButton>
+      </Link>
+    </EmptyCart>
+  );
+};
 
 export default Cart;
