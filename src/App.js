59558bf587f740cbb5a5e17ab7db26a9882ad87e
raych2 <rtongco@gmail.com>
2021-05-27 21:03:36 -0700

    Add CartContext to manage cartItems state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Shop from "./components/Shop";
 import Cart from "./components/Cart";
+import { CartProvider } from "./context/CartContext";
 import "./App.css";
 
 const App = () => {
-  const [cartItems, setCartItems] = useState([]);
-
-  const addToCart = (product, count) => {
-    let newItem = [...cartItems, { brand: product.brand, name: product.name, image: product.image, price: product.price, quantity: count }];
-    setCartItems(newItem);
-  }
-
   return (
     <Router>
       <div className="App">
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/shop" render={() => <Shop cartItems={cartItems} addToCart={addToCart}/>} />
-          <Route path="/cart" render={() => <Cart cartItems={cartItems} addToCart={addToCart}/>} />
-        </Switch>
+        <CartProvider>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/shop" component={Shop} />
+            <Route path="/cart" component={Cart} />
+          </Switch>
+        </CartProvider>
       </div>
     </Router>
   );
