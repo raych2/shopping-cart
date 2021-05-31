133add07b03ee5aee2c42096e6b41e731771fb74
raych2 <rtongco@gmail.com>
2021-05-31 15:21:25 -0700

    Update CartContext to include total state

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -4,8 +4,11 @@ export const CartContext = createContext();
 
 export const CartProvider = (props) => {
   const [cartItems, setCartItems] = useState([]);
+  const [total, setTotal] = useState(0);
   return (
-    <CartContext.Provider value={[cartItems, setCartItems]}>
+    <CartContext.Provider
+      value={{ cartItems: [cartItems, setCartItems], total: [total, setTotal] }}
+    >
       {props.children}
     </CartContext.Provider>
   );
